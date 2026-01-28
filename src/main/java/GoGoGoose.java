d51513cb3338f9afb7bc534598ada1561cf00bb7
Linying <e1355166@u.nus.edu>
2026-01-28 22:19:53 +0800

    Add Level 2 add the ability to store and display

--- src/main/java/GoGoGoose.java
+++ src/main/java/GoGoGoose.java
@@ -1,31 +1,45 @@
 import java.util.Scanner;
 
 public class GoGoGoose {
-    private static final String LINE = "____________________________________________________________";
+    private static final String DIVIDER_LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner inputScanner = new Scanner(System.in);
 
-        System.out.println(LINE);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+        System.out.println(DIVIDER_LINE);
         System.out.println("Hello! I'm GoGoGoose");
         System.out.println("What can I do for you?");
-        System.out.println(LINE);
+        System.out.println(DIVIDER_LINE);
 
         while (true) {
-            String input = in.nextLine();
+            String input = inputScanner.nextLine();
 
-            System.out.println(LINE);
+            System.out.println(DIVIDER_LINE);
 
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(LINE);
+                System.out.println(DIVIDER_LINE);
                 break;
             }
 
-            System.out.println(input);
-            System.out.println(LINE);
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(DIVIDER_LINE);
+                continue;
+            }
+
+            tasks[taskCount] = input;
+            taskCount++;
+
+            System.out.println("added: " + input);
+            System.out.println(DIVIDER_LINE);
         }
 
-        in.close();
+        inputScanner.close();
     }
 }
