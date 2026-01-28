56a1c6efb89ad4f7285e85f6587e48302268b1f9
Linying <e1355166@u.nus.edu>
2026-01-28 22:04:31 +0800

    Add Level 1 echo function

--- src/main/java/GoGoGoose.java
+++ src/main/java/GoGoGoose.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
+
 public class GoGoGoose {
+    private static final String LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        // String logo = " ____        _        \n"
-        //         + "|  _ \\ _   _| | _____ \n"
-        //         + "| | | | | | | |/ / _ \\\n"
-        //         + "| |_| | |_| |   <  __/\n"
-        //         + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+
+        System.out.println(LINE);
         System.out.println("Hello! I'm GoGoGoose");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+
+        while (true) {
+            String input = in.nextLine();
+
+            System.out.println(LINE);
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(input);
+            System.out.println(LINE);
+        }
+
+        in.close();
     }
 }
