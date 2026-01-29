ed226faf14c4b39983b569c570a93f94d733d410
Linying <e1355166@u.nus.edu>
2026-01-29 22:03:02 +0800

    Add Level-3 task management with mark/unmark functions
    Implemented the ability for users to mark tasks as done and unmark them
    back to not done. Refactored task handling to use a separate Task class,
    following the A-Classes extension. Updated main program to support:
    
    * 'mark <number>' to mark a task as done
    * 'unmark <number>' to mark a task as not done
    * Safe handling of invalid indices to prevent crashes

--- src/main/java/GoGoGoose.java
+++ src/main/java/GoGoGoose.java
@@ -6,7 +6,7 @@ public class GoGoGoose {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println(DIVIDER_LINE);
@@ -15,28 +15,51 @@ public class GoGoGoose {
         System.out.println(DIVIDER_LINE);
 
         while (true) {
-            String input = inputScanner.nextLine();
+            String userInput = inputScanner.nextLine().trim();
 
             System.out.println(DIVIDER_LINE);
 
-            if (input.equals("bye")) {
+            if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(DIVIDER_LINE);
                 break;
             }
 
-            if (input.equals("list")) {
+            if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
                 System.out.println(DIVIDER_LINE);
                 continue;
             }
 
-            tasks[taskCount] = input;
+            if (userInput.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[taskIndex]);
+                }
+                System.out.println(DIVIDER_LINE);
+                continue;
+            }
+
+            if (userInput.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    tasks[taskIndex].unmarkAsDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[taskIndex]);
+                }
+                System.out.println(DIVIDER_LINE);
+                continue;
+            }
+
+            tasks[taskCount] = new Task(userInput);
             taskCount++;
 
-            System.out.println("added: " + input);
+            System.out.println("added: " + userInput);
             System.out.println(DIVIDER_LINE);
         }
 
